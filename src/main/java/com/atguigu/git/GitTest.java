a61b2c660d5a97a3dd4c298e4bf88c76394317ed
Heyun <765330719@qq.com>
2022-04-04 20:29:19 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+
+        System.out.println("hello github3");
+        System.out.println("hello github4");
+        System.out.println("hot-fix test");
     }
 }
